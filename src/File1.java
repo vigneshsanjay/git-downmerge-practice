328e05b2995cd3ee55826bfa5ce87cacf8ca28b5
vigneshsanjay <55480752+vigneshsanjay@users.noreply.github.com>
2026-09-05 18:39:48 +0530

    SCA_vuln_fixes

--- src/File1.java
+++ src/File1.java
@@ -1,3 +1,4 @@
 This is the File 1 changes.
 this is the august release changes added here.
 These changes are related to the vulnerability fixing in file 1
+These changes are related to the SCA vulnerabilities of file 1
